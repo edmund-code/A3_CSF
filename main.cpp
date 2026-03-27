c1da71a3b9d8da30ee0eb63ae7653bf1ee619095
EvanBatten <ebatten28@gmail.com>
2026-03-27 00:34:16 -0400

    comments

--- main.cpp
+++ main.cpp
@@ -8,89 +8,96 @@ int main( int argc, char **argv ) {
   // TODO: implement  
   if (argc != 7) {
     
-    std::cerr << "Usage: ./csim num_sets blocks_per_set block_size write_policy alloc_policy eviction_policy" << std::endl;
+    std::cerr << "./csim numSet blocksPer blockSize writePol allocPol evictPol, you did this wrong" << std::endl;
     
     return 1;
   }
 
-  uint32_t num_sets = atoi(argv[1]);
-  uint32_t blocks_per_set = atoi(argv[2]);
-  uint32_t block_size = atoi(argv[3]);
-  std::string alloc_policy = argv[4];
-  std::string write_policy = argv[5];
-  std::string eviction_policy = argv[6];
+  //set and parse all the args
+  uint32_t numSet = atoi(argv[1]);
+  uint32_t blocksPer = atoi(argv[2]);
+  uint32_t blockSize = atoi(argv[3]);
 
-  //power of 2
-  if (num_sets == 0 || (num_sets & (num_sets - 1)) != 0) {
+  std::string allocPol = argv[4];
+  std::string writePol = argv[5];
+  std::string evictPol = argv[6];
+
+  //power of 2 correction
+  if (numSet == 0 || (numSet & (numSet - 1)) != 0) {
     
-    std::cerr << "Error: num_sets must be a positive power of 2" << std::endl;
+    std::cerr << "numSet must be pos power of 2" << std::endl;
     return 1;
   }
 
-  if (blocks_per_set == 0 || (blocks_per_set & (blocks_per_set - 1)) != 0) {
+  if (blocksPer == 0 || (blocksPer & (blocksPer - 1)) != 0) {
     
-    std::cerr << "Error: blocks_per_set must be a positive power of 2" << std::endl;
+    std::cerr << "blocksFer pos power of 2" << std::endl;
     return 1;
   }
 
-  if (block_size < 4 || (block_size & (block_size - 1)) != 0) {
+  if (blockSize < 4 || (blockSize & (blockSize - 1)) != 0) {
     
-    std::cerr << "Error: block_size must be a positive power of 2 and at least 4" << std::endl;
+    std::cerr << "blockSize not positive power of 2 and at least 4" << std::endl;
     return 1;
 
   }
 
-  //string arguments
-  if (alloc_policy != "write-allocate" && alloc_policy != "no-write-allocate") {
+  //string args check
+  if (allocPol != "write-allocate" && allocPol != "no-write-allocate") {
     
-    std::cerr << "Error: invalid allocation policy" << std::endl;
+    std::cerr << "invalid alloc policy" << std::endl;
     return 1;
 
   }
-  if (write_policy != "write-through" && write_policy != "write-back") {
+  if (writePol != "write-through" && writePol != "write-back") {
     
-    std::cerr << "Error: invalid write policy" << std::endl;
+    std::cerr << "invalid write pol." << std::endl;
     return 1;
  
   }
   
-  if (eviction_policy != "lru" && eviction_policy != "fifo") {
+  if (evictPol != "lru" && evictPol != "fifo") {
   
-    std::cerr << "Error: invalid eviction policy" << std::endl;
+    std::cerr << "invalid evict pol" << std::endl;
     return 1;
   
   }
 
   //write alloc and write back not allow
-  if (alloc_policy == "no-write-allocate" && write_policy == "write-back") {
+  if (allocPol == "no-write-allocate" && writePol == "write-back") {
     
-    std::cerr << "Error: no-write-allocate cannot be combined with write-back" << std::endl;
+    std::cerr << "cant do both alloc and write" << std::endl;
     return 1;
   
   }
 
-  bool write_allocate = (alloc_policy == "write-allocate");
-  bool write_back = (write_policy == "write-back");
-  bool is_lru = (eviction_policy == "lru");
+  //flags for if alloc write, write back, and isLru
+  bool writeAlloc = (allocPol == "write-allocate");
+  bool writeBack = (writePol == "write-back");
+  bool isLru = (evictPol == "lru");
 
-  Cache cache(num_sets, blocks_per_set, block_size, write_allocate, write_back, is_lru);
+  Cache cache(numSet, blocksPer, blockSize, writeAlloc, writeBack, isLru);
 
-  std::string line;
-  while (std::getline(std::cin, line)) {
+  //get each line
+  std::string tmp;
+  while (std::getline(std::cin, tmp)) {
     
-    if (line.empty()) {
+    if (tmp.empty()) {
       continue;
 
     };
 
+    //vars for the op, address, etc.
     char operation;
     uint32_t address;
     int ignored;
-
-    if (sscanf(line.c_str(), " %c 0x%x %d", &operation, &address, &ignored) != 3) {
+    
+    //validates the input line, if we dont have 3 then not valid so we should not load or store
+    if (sscanf(tmp.c_str(), " %c 0x%x %d", &operation, &address, &ignored) != 3) {
       continue;
     }
 
+    //decides load or store op based on flag
     if (operation == 'l') {
      
       cache.load(address);
